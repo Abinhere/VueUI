a8ce14d5c03b9946ce9986db00962cab21a43d9a
Abinhere <1538380910@qq.com>
2020-12-22 20:48:39 +0800

    小修改

--- abin-vueui-1/src/components/ButtonDemo.vue
+++ abin-vueui-1/src/components/ButtonDemo.vue
@@ -4,12 +4,20 @@
     </div>
     <h1>示例1</h1>
     <div>
-        <Button>哇嘎雷共</Button>
+        <Button @click="onClick" @focus="onClick" @mouseover="onClick">
+            哇嘎雷共
+        </Button>
     </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-    components:{Button}
+    components:{Button},
+    setup() {
+        const onClick = ()=>{
+            console.log('h1');
+        }
+        return {onClick};
+    }
 }
 </script>
