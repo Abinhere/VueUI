3291e3c7e3855aa719f4566f095cef6c229df29d
Abinhere <1538380910@qq.com>
2020-12-25 14:20:52 +0800

    创建Tabs和Tab组件

--- abin-vueui-1/src/views/Doc.vue
+++ abin-vueui-1/src/views/Doc.vue
@@ -11,9 +11,6 @@
                     <router-link to="/doc/button">Button 组件</router-link>
                 </li>
                 <li>
-                    <router-link to="/doc/dialog">Dialog 组件</router-link>
-                </li>
-                <li>
                     <router-link to="/doc/tabs">Tabs 组件</router-link>
                 </li>
             </ol>
